eaab2c30743b2c40a11b613a5876d74a0d012605
Elena <parfyo-elena@yandex.ru>
2024-03-16 19:37:23 +0300

    added banner-slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,22 @@
 $(function () {
 
   $('.main-slider__wrapper').slick({
-    prevArrow: '<button type="button" class="slick-prev"><img src="images/main-slider/arrows/arrow-left.svg" alt="arrow-left"></button>',
-
-    nextArrow: '<button type="button" class="slick-next"><img src="images/main-slider/arrows/arrow-right.svg" alt="arrow-right"></button>',
+    infinite: false,
     autoplay: true,
     autoplaySpeed: 4500,
     // fade: true,
     cssEase: 'ease-out',
   });
+
+  $('.banner-slider__wrapper').slick({
+    arrows: false,
+    slidesToShow:1,
+    pauseOnHover: true,
+    // autoplay: true,
+    variableWidth: true,
+  });
+
+
 
   $(".star").rateYo({
     starWidth: "16px",

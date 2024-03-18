0edd755aa3acfcfebcd255cb6514ff57976106ae
Elena <parfyo-elena@yandex.ru>
2024-03-18 19:32:02 +0300

    fix all sections

--- app/js/main.js
+++ app/js/main.js
@@ -1,22 +1,28 @@
 $(function () {
 
+  $('.burger-catalog, .catalog-menu__link').on('click', function () {
+    $('.catalog-menu').toggleClass('catalog-menu--active');
+    $('.burger-catalog').toggleClass('burger-catalog--active');
+
+  });
+
+
   $('.main-slider__wrapper').slick({
     infinite: false,
-    autoplay: true,
+    // autoplay: true,
     autoplaySpeed: 4500,
-    // fade: true,
     cssEase: 'ease-out',
   });
+
 
   $('.banner-slider__wrapper').slick({
     arrows: false,
     slidesToShow: 1,
     pauseOnHover: true,
-    autoplay: true,
+    // autoplay: true,
     variableWidth: true,
     dots: true,
   });
-
 
 
   $(".star").rateYo({
@@ -25,15 +31,7 @@ $(function () {
     ratedFill: "#FFB800",
     numStars: 1,
     readOnly: true,
-
-    // onChange: function (rating, rateYoInstance) {
-
-    //   $(this).next().text(rating);
-    // }
-
   });
-
-
 
 
   var containerEl1 = document.querySelector('[data-ref="container-1"]');

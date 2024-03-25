9345d3e7e52f3401be4d1232900746848fc79459
Elena <parfyo-elena@yandex.ru>
2024-03-25 11:28:40 +0300

    fix all section, added brand-slider, delete banner-slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,27 +1,41 @@
 $(function () {
 
-  $('.burger-catalog, .catalog-menu__link').on('click', function () {
+  $(document).mouseup(function (e) {
+    var menu = $(".catalog-menu");
+    var burger = $(".burger-catalog");
+
+    if (!menu.is(e.target) // если клик был не по нашему блоку
+      && menu.has(e.target).length === 0) { // и не по его дочерним элементам
+      menu.removeClass('catalog-menu--active');
+      burger.removeClass('burger-catalog--active');
+    }
+
+    $('.burger-catalog').on('click', function () {
+      $('.catalog-menu').toggleClass('catalog-menu--active');
+      $('.burger-catalog').toggleClass('burger-catalog--active');
+    });
+  });
+
+  $('.catalog-menu__link').on('click', function () {
     $('.catalog-menu').toggleClass('catalog-menu--active');
     $('.burger-catalog').toggleClass('burger-catalog--active');
-
   });
+
 
 
   $('.main-slider__wrapper').slick({
     infinite: false,
-    // autoplay: true,
     autoplaySpeed: 4500,
     cssEase: 'ease-out',
   });
 
 
-  $('.banner-slider__wrapper').slick({
+  $('.brand-slider__items').slick({
     arrows: false,
-    slidesToShow: 1,
-    pauseOnHover: true,
-    // autoplay: true,
-    variableWidth: true,
     dots: true,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
   });
 
 
@@ -34,8 +48,8 @@ $(function () {
   });
 
 
-  var containerEl1 = document.querySelector('[data-ref="container-1"]');
-  var containerEl2 = document.querySelector('[data-ref="container-2"]');
+  var containerEl1 = document.querySelector('[data-ref="top-products"]');
+  var containerEl2 = document.querySelector('[data-ref="promotions"]');
 
   var config = {
     controls: {
@@ -47,4 +61,7 @@ $(function () {
   var mixer2 = mixitup(containerEl2, config);
 
 });
+
+
+
 
